59675d06e2a2e483fcbcce045b5add00a6fa4ec0
Paul Schwarzenberger <paul@celidor.net>
2021-06-14 10:24:59 +0100

    SE-1719 specify all provider versions

--- provider.tf
+++ provider.tf
@@ -6,7 +6,15 @@ terraform {
   required_providers {
     aws = {
       source  = "hashicorp/aws"
-      version = "~> 3.39"
+      version = "~> 3.43"
+    }
+    archive = {
+      source  = "hashicorp/archive"
+      version = "~> 2.2.0"
+    }
+    null = {
+      source  = "hashicorp/null"
+      version = "~> 3.1.0"
     }
   }
 }
