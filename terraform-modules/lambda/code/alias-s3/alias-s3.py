ff5db8f91224ebfe36942fc589f24ca0688b69b4
Paul Schwarzenberger <paul@celidor.net>
2022-01-07 15:59:52 +0000

    OSE-341 python formatting

--- terraform-modules/lambda/code/alias-s3/alias-s3.py
+++ terraform-modules/lambda/code/alias-s3/alias-s3.py
@@ -53,9 +53,7 @@ def lambda_handler(event, context):  # pylint:disable=unused-argument
                 if result:
                     print(f"{record['Name']} in {account_name} is vulnerable")
                     vulnerable_domains.append(record["Name"])
-                    takeover_domain = (
-                        record["Name"] + "s3-website." + record["AliasTarget"]["DNSName"].split("-", 2)[2]
-                    )
+                    takeover_domain = record["Name"] + "s3-website." + record["AliasTarget"]["DNSName"].split("-", 2)[2]
                     json_data["Findings"].append(
                         {
                             "Account": account_name,
