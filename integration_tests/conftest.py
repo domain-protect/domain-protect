ae7a7323788fac53b9dcf79ee72f362dc30c67fa
rob <rob.edwards@ovoenergy.com>
2022-07-15 15:16:47 +0100

    OSE-933 - Created DNS mock

--- integration_tests/conftest.py
+++ integration_tests/conftest.py
@@ -1,6 +1,7 @@
 import pytest
 from unittest.mock import patch
-from integration_tests.manual_scans.cloudflare.mock.cloudflare_mock import CloudFlareMock
+from integration_tests.mocks.cloudflare_mock import CloudFlareMock
+from integration_tests.mocks.dns_mock import DNSMock
 
 
 @pytest.fixture
@@ -8,4 +9,12 @@ def cloudflare_mock():
     with patch("CloudFlare.CloudFlare") as cf_mock:
         mock = CloudFlareMock()
         cf_mock.return_value = mock
+        yield mock
+
+
+@pytest.fixture
+def dns_mock():
+    with patch("dns.resolver.resolve") as monkey_patch:
+        mock = DNSMock()
+        monkey_patch.side_effect = mock.generate_lookup_function()
         yield mock
--- integration_tests/manual_scans/cloudflare/test_manual_cf_ns.py
+++ integration_tests/manual_scans/cloudflare/test_manual_cf_ns.py
@@ -5,9 +5,8 @@ from manual_scans.cloudflare.cf_ns import main
 
 
 @patch("manual_scans.cloudflare.cf_ns.print_list")
-@patch("dns.resolver.resolve")
-def test_cf_ns_detects_ns_with_no_name_server(dns_mock, print_list_mock, cloudflare_mock):
-    dns_mock.side_effect = dns.resolver.NoNameservers
+def test_cf_ns_detects_ns_with_no_name_server(print_list_mock, cloudflare_mock, dns_mock):
+    dns_mock.add_lookup("sub.ns.co.uk", "sub.ns.co.uk", exception=dns.resolver.NoNameservers)
 
     cloudflare_mock.add_zone("test1.co.uk").add_dns("test1.co.uk", "NS", "ns.test.co.uk").add_dns(
         "sub.ns.co.uk", "NS", "sub.ns.co.uk"
