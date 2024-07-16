5c869fc5a7c28c6d926760a453f579228cdf3282
Alexis Revelo <129888160+arevelo-bc@users.noreply.github.com>
2024-07-16 16:39:04 -0600

    Update locals.tf to allow the takeover to be implemented in our QA instance instead than our PROD

--- locals.tf
+++ locals.tf
@@ -1,4 +1,5 @@
 locals {
   env      = lower(terraform.workspace)
-  takeover = var.takeover == true && local.env == var.production_workspace ? true : false
+  # takeover = var.takeover == true && local.env == var.production_workspace ? true : false
+  takeover = var.takeover == true && local.env == "qa" ? true : false
 }
