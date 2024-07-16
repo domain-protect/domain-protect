b29d50952f6cb83b9ecf8eb0607edadaefedf567
Alexis Revelo <129888160+arevelo-bc@users.noreply.github.com>
2024-07-16 16:51:27 -0600

    Rollback to original as we have created the release for testing takeover in QA

--- locals.tf
+++ locals.tf
@@ -1,5 +1,4 @@
 locals {
   env      = lower(terraform.workspace)
-  # takeover = var.takeover == true && local.env == var.production_workspace ? true : false
-  takeover = var.takeover == true && local.env == "qa" ? true : false
+  takeover = var.takeover == true && local.env == var.production_workspace ? true : false
 }
