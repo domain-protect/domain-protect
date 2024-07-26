08c2eb42118942a6dddac4696f52e0f78b704297
Adam Pietrzycki <adam@pietrzycki.com>
2024-07-26 22:12:54 +0100

    Refactor env local variable to input variable

--- terraform-modules/lambda-resources/main.tf
+++ terraform-modules/lambda-resources/main.tf
@@ -20,7 +20,7 @@ resource "aws_lambda_function" "lambda" {
   for_each = toset(var.lambdas)
 
   filename         = "${path.cwd}/build/resources.zip"
-  function_name    = "${var.project}-resources-${local.env}"
+  function_name    = "${var.project}-resources-${var.env}"
   description      = "${var.project} lists resources created to prevent hostile takeover"
   role             = var.lambda_role_arn
   handler          = "resources.lambda_handler"
@@ -50,8 +50,8 @@ resource "aws_lambda_function" "lambda" {
 resource "aws_lambda_alias" "lambda" {
   for_each = toset(var.lambdas)
 
-  name             = "${var.project}-${each.value}-${local.env}"
-  description      = "Alias for ${var.project}-${each.value}s-${local.env}"
+  name             = "${var.project}-${each.value}-${var.env}"
+  description      = "Alias for ${var.project}-${each.value}s-${var.env}"
   function_name    = aws_lambda_function.lambda[each.key].function_name
   function_version = "$LATEST"
 }
