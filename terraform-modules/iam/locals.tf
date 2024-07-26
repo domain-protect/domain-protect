1bc26b03c4672657a3aece5b15c079cf9ece4b46
Adam Pietrzycki <adam@pietrzycki.com>
2024-07-26 22:56:37 +0100

    Remove locals.tf and inline local variables

--- terraform-modules/iam/locals.tf
+++
@@ -1,9 +0,0 @@
-locals {
-  role_name              = var.takeover ? "takeover" : local.role_policy_name
-  role_policy_name       = var.role_name == "policyname" ? var.policy : var.role_name
-  dynamodb_table_name    = "${replace(title(replace(var.project, "-", " ")), " ", "")}VulnerableDomains${title(var.env)}"
-  dynamodb_ip_table_name = "${replace(title(replace(var.project, "-", " ")), " ", "")}IPs${title(var.env)}"
-  account_id             = data.aws_caller_identity.current.account_id
-  dynamodb_arn           = "arn:aws:dynamodb:${var.region}:${local.account_id}:table/${local.dynamodb_table_name}"
-  dynamodb_ip_arn        = "arn:aws:dynamodb:${var.region}:${local.account_id}:table/${local.dynamodb_ip_table_name}"
-}
--- terraform-modules/iam/main.tf
+++ terraform-modules/iam/main.tf
@@ -1,5 +1,5 @@
 resource "aws_iam_role" "lambda" {
-  name = "${var.project}-${local.role_name}-${var.env}"
+  name = "${var.project}-${var.takeover ? "takeover" : var.role_name == "policyname" ? var.policy : var.role_name}-${var.env}"
   assume_role_policy = templatefile("${path.module}/templates/${var.assume_role_policy}_role.json.tpl", {
     project = var.project,
   })
@@ -8,15 +8,15 @@ resource "aws_iam_role" "lambda" {
 }
 
 resource "aws_iam_role_policy" "lambda" {
-  name = "${var.project}-${local.role_policy_name}-${var.env}"
+  name = "${var.project}-${var.role_name == "policyname" ? var.policy : var.role_name}-${var.env}"
   role = aws_iam_role.lambda.id
   policy = templatefile("${path.module}/templates/${var.policy}_policy.json.tpl", {
     security_audit_role_name = var.security_audit_role_name,
     project                  = var.project,
     env                      = var.env,
     kms_arn                  = var.kms_arn,
-    ddb_table_arn            = local.dynamodb_arn,
-    ddb_ip_table_arn         = local.dynamodb_ip_arn,
+    ddb_table_arn            = "arn:aws:dynamodb:${var.region}:${data.aws_caller_identity.current.account_id}:table/${replace(title(replace(var.project, "-", " ")), " ", "")}VulnerableDomains${title(var.env)}",
+    ddb_ip_table_arn         = "arn:aws:dynamodb:${var.region}:${data.aws_caller_identity.current.account_id}:table/${replace(title(replace(var.project, "-", " ")), " ", "")}IPs${title(var.env)}",
     state_machine_arn        = var.state_machine_arn,
   })
 }
