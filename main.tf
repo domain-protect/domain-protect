74012049192b765f848e51a275719a0ed1badaf5
Adam Pietrzycki <adam@pietrzycki.com>
2024-07-27 10:02:49 +0100

    Replace local env with var environment for consistency
    Signed-off-by: Adam Pietrzycki <adam@pietrzycki.com>

--- main.tf
+++ main.tf
@@ -121,6 +121,7 @@ module "lambda-takeover" {
   kms_arn           = module.kms.kms_arn
   sns_topic_arn     = module.sns.sns_topic_arn
   dlq_sns_topic_arn = module.sns-dead-letter-queue.sns_topic_arn
+  environment       = local.env
 }
 
 module "takeover-role" {
